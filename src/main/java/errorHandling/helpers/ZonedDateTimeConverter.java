83d7f8bfaf8060b761ff8f31c4cf945a19602634
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-09-10 18:10:39 -0500

    added the ZonedDateTimeEnum

--- src/main/java/errorHandling/helpers/ZonedDateTimeConverter.java
+++ src/main/java/errorHandling/helpers/ZonedDateTimeConverter.java
@@ -2,11 +2,12 @@ package errorHandling.helpers;
 
 import java.io.IOException;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+
+import enums.ZonedDateTimeEnum;
 
 public class ZonedDateTimeConverter
 	extends JsonSerializer<ZonedDateTime>
@@ -18,8 +19,7 @@ public class ZonedDateTimeConverter
 	{
 		if (null != value)
 		{
-            DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss z (XXX)");
-            String zonedDateTimeAsString = value.format(formatter1);
+			String zonedDateTimeAsString = ZonedDateTimeEnum.INSTANCE.writeDateString(value, ZonedDateTimeEnum.INSTANCE.DATE_FORMAT3);
             
 			jgen.writeString(zonedDateTimeAsString);
 		}
